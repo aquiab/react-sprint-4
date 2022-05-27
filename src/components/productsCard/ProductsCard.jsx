3265694cefd2d41efcb9f4a3938cfeab3c4ad13f
Rojas Lara <A446482@santandertecnologia.com.ar>
2022-05-27 17:47:59 -0300

    productsCard update

--- src/components/productsCard/ProductsCard.jsx
+++ src/components/productsCard/ProductsCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './ProductsCard.css'
+import svgChevron from '../../assets/chevron-right.svg'
 
 const ProductsCard = () => {
+
+
   return (
-    <div>ProductsCard</div>
+    <>
+    <ul className='barra-products'>
+        <div className='product'>
+            <li><img src='' alt="imgProduct" /></li>
+                <li className='product-info-container'> 
+                    <div className='product-info'>
+                        <p>
+                            Coca Cola Lara 220ml Original Packx8
+                        </p>
+                        <p className='idProduct'>#123123</p>
+                    </div>  
+                </li>
+        </div>
+        <li className='flecha-container'><img className='flecha' src={svgChevron} alt="imgProduct" />  </li>
+    </ul>
+    </>
   )
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import BarraMain from "../../components/barra-main/BarraMain"
 import "./Home.css"
 import svgProduct from '../../assets/package-variant-closed.svg'
 import svgStore from '../../assets/store.svg'
+import ProductsCard from "../../components/productsCard/ProductsCard"
 
 export default function Home() {
 
@@ -10,7 +11,7 @@ export default function Home() {
     return(
         <>
         <BarraMain svgImg={svgProduct}/>
-        <BarraMain svgImg={svgStore}/>
+        <BarraMain svgImg={svgStore}/> 
         </>
     )
 }
