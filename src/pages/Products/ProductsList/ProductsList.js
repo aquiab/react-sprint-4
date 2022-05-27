12765ace982898afd0f37369aa57de02f382f60d
Rojas Lara <A446482@santandertecnologia.com.ar>
2022-05-27 19:00:20 -0300

    get db update

--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,10 +1,31 @@
 import "./ProductsList.css"
 import ProductsCard from '../../../components/productsCard/ProductsCard'
+import axios from 'axios';
+import { useState , useEffect} from "react";
 
 export default function ProductsList() {
+
+    const [productos , setProductos]= useState([])
+
+    useEffect (()=>{
+
+        axios.get('http://localhost:3001/products/')
+            .then(res=> setProductos(res.data))
+            .catch(err=>console.log(err))
+    },[])
+     
     return(
         <>
-            <ProductsCard/>
+            <ul>
+                {productos.map((producto)=>
+                
+                    <li key={producto.id}>
+                        <ProductsCard producto={producto}/>
+                    </li>
+                )}
+            </ul>
+
+            
         </>
     )
 }
