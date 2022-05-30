de0e4053958f0a13b4bc437da91bc69735113970
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-05-30 09:52:56 -0300

    Agregue contenido a la pagina de error

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,8 +1,15 @@
+import Header from "../../components/Header/Header"
 import "./Error.css"
 
 export default function Error() {
+    const crumbs = [{ title: "Página no encontrada", path: "#" }];
     return(
-        <div class="error-msg">
+        <div className="errorPage__container">
+            <Header breadcrums={crumbs}/>
+            <div className="errorPage__content">
+                <span className="code">404</span>
+                <h1>La página que estás buscando no existe.</h1>
+            </div>
             Página no encontrada
         </div>
     )
