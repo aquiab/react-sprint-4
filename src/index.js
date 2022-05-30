7da6105ba3f4b0d70eeb85cbb36bfd9027b04e45
Garbett <A446488@santandertecnologia.com.ar>
2022-05-30 13:33:10 -0300

    mal commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+mport React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,16 +6,20 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import SidebarProvider from "./context/sidebarContext";
 import UserProvider from "./context/userContext";
+import {ThemeProvider} from "./context/ThemeProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
+
 		<BrowserRouter>
+			<ThemeProvider>
 			<UserProvider>
 				<SidebarProvider>
 					<App />
 				</SidebarProvider>
 			</UserProvider>
+			</ThemeProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
