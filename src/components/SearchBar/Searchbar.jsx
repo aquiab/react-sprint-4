35e6c70444bfb0aa71b78991cad40474aac6c3e8
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-05-30 14:11:02 -0300

    Agregue el searchbar

---
+++ src/components/SearchBar/Searchbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import svgLupa from "../../assets/lupa.svg";
+import "./searchbar.css";
+const Searchbar = ({ value, onChange, onExpand }) => {
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const handleClick = () => {
+		setIsExpanded((x) => !x);
+    onExpand();
+	};
+	return (
+			<div className="searchbar-container"><button
+				className="closeButton"
+				style={{ display: isExpanded ? "flex" : "none" }}
+        onClick={handleClick}
+			>
+				X
+			</button>
+		<div className={`searchBar ${isExpanded && "expanded"}`}>
+			<form>
+				<input
+					type="text"
+					id="busqueda"
+					name="busqueda"
+					value={value}
+					onChange={onChange}
+					placeholder="Buscar productos"
+				/>
+			</form>
+			<button className="lupa" onClick={handleClick}>
+				<img src={svgLupa} alt="" />
+			</button>
+		</div>
+    </div>
+	);
+};
+
+export default Searchbar;
--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,70 +1,69 @@
-import "./ProductsList.css"
-import ProductsCard from '../../../components/productsCard/ProductsCard'
-import { useContext, useEffect , useState } from "react";
+import "./ProductsList.css";
+import ProductsCard from "../../../components/productsCard/ProductsCard";
+import { useContext, useEffect, useState } from "react";
 import useGetProducts from "../../../hooks/useGetProducts";
 import { productsContext } from "../../../context/ProductsContext";
-import axios from 'axios'
+import axios from "axios";
 import Header from "../../../components/Header/Header";
+import Searchbar from "../../../components/SearchBar/Searchbar";
 
 export default function ProductsList() {
+	const crumbs = [{ title: `Productos`, path: "/products" }];
 
-    const crumbs = [{ title: `Productos`, path: "/products" }];
+	const { setProducts } = useContext(productsContext);
 
-    const { setProducts }= useContext(productsContext)
+	const [busqueda, setBusqueda] = useState("");
 
-    const [busqueda , setBusqueda] = useState('');
+	const [filtrados, setFiltrados] = useState([]);
 
-    const [filtrados, setFiltrados] = useState([]);
+	const { products, getProducts } = useGetProducts();
 
-    const {products, getProducts} = useGetProducts();
+    const [searchbarExpanded, setSearchbarExpanded] = useState(false)
 
-    const handleChange = (e) =>{
-        setBusqueda(e.target.value);
-        filtrar(e.target.value)
+	const handleChange = (e) => {
+		setBusqueda(e.target.value);
+		filtrar(e.target.value);
+	};
+
+	const filtrar = (terminoBusqueda) => {
+		var filt = filtrados.filter((elemento) => {
+			if (
+				elemento.nombre.toLowerCase().includes(terminoBusqueda.toLowerCase())
+			) {
+				return true;
+			}
+			return false;
+		});
+		setProducts(filt);
+	};
+
+	useEffect(() => {
+		getProducts();
+		axios
+			.get("http://localhost:3001/products/")
+			.then((res) => setFiltrados(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+
+
+    const handleSearchBarExpand = () => {
+        setSearchbarExpanded(!searchbarExpanded)
     }
+	return (
+		<>
+			<Header
+				breadcrumbs={crumbs}
+				addon={<Searchbar value={busqueda} onChange={handleChange} onExpand={handleSearchBarExpand}/>}
+				addonExpanded={searchbarExpanded}
+			/>
 
-    const filtrar = (terminoBusqueda) => {
-        var filt= filtrados.filter((elemento=>{
-            if(elemento.nombre.toLowerCase().includes(terminoBusqueda.toLowerCase())){
-                return true;
-            }
-            return false
-        }))
-        setProducts(filt)
-    }
-
-    useEffect (()=>{
-
-        getProducts();
-        axios.get('http://localhost:3001/products/')
-            .then(res=>setFiltrados(res.data))
-            .catch(err=>console.log(err))
-    },[])
-    
-    return(
-        <>
-        <Header breadcrumbs={crumbs}/>
-        <form>
-            <label htmlFor='busqueda'>Buscar</label> 
-            <input 
-                type='text' 
-                id='busqueda' 
-                name='busqueda' 
-                value={busqueda} 
-                onChange={handleChange}
-        />
-        </form>
-        
-            <ul>
-                {products.map((producto)=>
-                
-                    <li key={producto.id}>
-                        <ProductsCard producto={producto}/>
-                    </li>
-                )}
-            </ul>
-        
-            
-        </>
-    )
+			<ul>
+				{products.map((producto) => (
+					<li key={producto.id}>
+						<ProductsCard producto={producto} />
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
