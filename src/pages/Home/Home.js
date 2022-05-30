d0a25e87dc97f907cf9780417d3df7763d92056c
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-05-30 10:03:14 -0300

    Agregue el header y corregi estilo en Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,22 @@
-import BarraMain from "../../components/barra-main/BarraMain"
-import "./Home.css"
-import svgProduct from '../../assets/package-variant-closed.svg'
-import svgStore from '../../assets/store.svg'
-import ProductsCard from "../../components/productsCard/ProductsCard"
+import BarraMain from "../../components/barra-main/BarraMain";
+import "./Home.css";
+import svgProduct from "../../assets/package-variant-closed.svg";
+import svgStore from "../../assets/store.svg";
+import Header from "../../components/Header/Header";
+import { useContext } from "react";
+import { userContext } from "../../context/userContext";
 
 export default function Home() {
+	const { user } = useContext(userContext);
 
-
-
-    return(
-        <>
-        <BarraMain svgImg={svgProduct}/>
-        <BarraMain svgImg={svgStore}/> 
-        </>
-    )
+	const crumbs = [{ title: `Hola ${user.name}!`, path: "/" }];
+	return (
+		<div className="home">
+			<Header breadcrums={crumbs} />
+			<div className="home__container">
+				<BarraMain svgImg={svgProduct} />
+				<BarraMain svgImg={svgStore} />
+			</div>
+		</div>
+	);
 }
