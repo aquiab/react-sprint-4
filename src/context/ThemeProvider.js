c870735552f93b79f08520438872903fe08511db
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-05-30 14:33:51 -0300

    Correccion ThemeProvider

--- src/context/ThemeProvider.js
+++ src/context/ThemeProvider.js
@@ -3,14 +3,30 @@ import React, { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = (props) => {
-    
-    const [darkMode, setDarkMode] = useState(
-        JSON.parse(localStorage.getItem("darkMode"))
-    );
+	const [darkMode, setDarkMode] = useState(
+		JSON.parse(localStorage.getItem("darkMode"))
+	);
 
-    return (
-        <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-            {props.children}
-        </ThemeContext.Provider>
-    );
+	const root = document.querySelector(":root");
+
+	const ligth = `
+    --imagenBarraMain:invert(100%);
+    --fondoColorBarraMain:grey;
+    --colorFuenteBarraMain:white;
+    --colorFuenteBarraButton:white;
+    --colorFondoBarraButton:rgb(167, 167, 167);`;
+    
+	const dark =
+		`--imagenBarraMain:black;
+        --fondoColorBarraMain:black;
+        --colorFuenteBarraMain:white;
+        --colorFuenteBarraButton:white;
+        --colorFondoBarraButton:rgb(167, 167, 167);`;
+
+	root.style = darkMode ? dark : ligth;
+	return (
+		<ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+			{props.children}
+		</ThemeContext.Provider>
+	);
 };
