996b1de692e84bffa883ae7b4a6212e64edaea57
Aquiles <aquiabuin@gmail.com>
2022-05-30 10:14:53 -0300

    agregue editor de producto

---
+++ src/components/ProductImagePreview.js
@@ -0,0 +1,18 @@
+import "./ProductImagePreview.css"
+
+export default function ProductImagePreview({img, setForm}) {
+    const quitarImagen = e => {
+        e.preventDefault()
+        setForm(prevState => ({...prevState, imgs: prevState.imgs.filter(url => url !== img)}))
+    }
+
+    return(
+        <div className="image-preview">
+            <span className="image-preview-info">
+                <img src={img} alt={img}/>
+                <p>{img}</p>
+            </span>
+            <button onClick={quitarImagen}>Quitar</button>
+        </div>
+    )
+}
