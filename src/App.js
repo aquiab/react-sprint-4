11864dce338f21d521d46cc68da177db6bef1a88
Garbett <A446488@santandertecnologia.com.ar>
2022-05-30 13:53:10 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import MainArea from "./components/MainArea/MainArea";
 import AppRouter from "./routes/AppRouter";
 import ProductsProvider from "./context/ProductsContext";
+import ThemeButton from "../src/components/ThemeButton";
+
+
 
 function App() {
   return (
     <>
+
       <ProductsProvider>
         <MainArea>
         <AppRouter />
         </MainArea>
       </ProductsProvider>
+        <ThemeButton/>
     </>
   );
 }
--- src/components/barra-main/BarraMain.jsx
+++ src/components/barra-main/BarraMain.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import './BarraMain.css' 
 import { useEffect} from "react";
 import useGetProducts from "../../hooks/useGetProducts";
+import {ThemeContext} from "../../context/ThemeProvider";
 
 
 const BarraMain = (props) => {
@@ -14,17 +15,17 @@ const BarraMain = (props) => {
     getProducts();
     
   },[])
-
+    const { darkMode } = useContext(ThemeContext);
   return (
     <>
-        <ul className='barra-main'>
-            <div className='barra-porducts'>
-                <li><img className='imagen' src={props.svgImg} alt="img" /></li>
+        <ul className={darkMode ? 'barra-main .dark' : 'barra-main'} >
+            <div className={darkMode ? 'barra-porducts .dark' :'barra-porducts'}>
+                <li><img className={darkMode ? 'imagen .dark' : 'imagen'} src={props.svgImg} alt="img" /></li>
                 <li> {products.length} {props.nombre[0]} </li>
             </div>
-            <div className='barra-buttons'>
-                <li><Link to='products'><button className='barra-button'>Ver Listado </button></Link></li>
-                <li><Link to=''><button className='barra-button'>Agregar {props.nombre[1]}  </button> </Link> </li>
+            <div className={darkMode ? 'barra-buttons .dark' : 'barra-buttons'}>
+                <li><Link to='products'><button className={darkMode ? 'barra-button .dark':'barra-button'}>Ver Listado </button></Link></li>
+                <li><Link to=''><button className={darkMode ? 'barra-button .dark': 'barra-button'}>Agregar {props.nombre[1]}  </button> </Link> </li>
             </div>
         </ul>
     </>
