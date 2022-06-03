1a3643bae20250c4c128b12f404bc48ca74e3b06
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-06-03 11:51:23 -0300

    Test: breadcrumb redirige al lugar indicado

--- src/components/Breadcrumb/breadcrumb.test.jsx
+++ src/components/Breadcrumb/breadcrumb.test.jsx
@@ -3,10 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import Breadcrumb from "./Breadcrumb";
 
 test("renders correctly", () => {
-	render(
-		<BrowserRouter>
-			<Breadcrumb steps={[{ title: "hola", path: "/" }]} />
-		</BrowserRouter>
-	);
+	render(<Breadcrumb steps={[{ title: "hola", path: "/" }]} />, {
+		wrapper: BrowserRouter,
+	});
 	expect(screen.getByText(/hola/i)).toBeInTheDocument();
+	expect(screen.getByText(/hola/).closest("a")).toHaveAttribute("href", "/");
 });
