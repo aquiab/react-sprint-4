e94d743bec5c715f87675ee546dc0c79a7102cba
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-06-03 16:45:59 -0300

    Test: MainArea renderiza

---
+++ src/components/MainArea/MainArea.test.jsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import { MemoryRouter } from "react-router-dom";
+import { sidebarContext } from "../../context/sidebarContext";
+import UserProvider from "../../context/userContext";
+import Header from "../Header/Header";
+import MainArea from "./MainArea";
+
+describe("Main Area", () => {
+	beforeEach(() => {
+		render(
+			<MemoryRouter>
+				<sidebarContext.Provider
+					value={{
+						isOpen: false,
+            toggleSidebar: jest.fn(),
+            openSidebar: jest.fn(),
+            closeSidebar: jest.fn(),
+					}}
+				>
+					<UserProvider>
+						<MainArea>
+							<Header />
+						</MainArea>
+					</UserProvider>
+				</sidebarContext.Provider>
+			</MemoryRouter>
+		);
+	});
+	test("Main area renderiza", () => {
+		let main = screen.getByRole("main");
+		let aside = screen.getByRole("complementary");
+		expect(main).toBeInTheDocument();
+		expect(aside).toBeInTheDocument();
+	});
+
+	
+});
