47ef4090e3f06e9b80a690e6788f5ff74a8e5e7c
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-06-06 10:21:21 -0300

    Actualice el test de Breadcrumb para que testee la cantidad de links

--- src/components/Breadcrumb/breadcrumb.test.jsx
+++ src/components/Breadcrumb/breadcrumb.test.jsx
@@ -3,9 +3,17 @@ import { BrowserRouter } from "react-router-dom";
 import Breadcrumb from "./Breadcrumb";
 
 test("renders correctly", () => {
-	render(<Breadcrumb steps={[{ title: "hola", path: "/" }]} />, {
+	const steps = [
+		{ title: "step1", path: "/" },
+		{ title: "step2", path: "/step2" },
+		{ title: "step3", path: "/step3" },
+		{ title: "step4", path: "/step4" },
+		{ title: "step5", path: "/step5" },
+	];
+	render(<Breadcrumb steps={steps} />, {
 		wrapper: BrowserRouter,
 	});
-	expect(screen.getByText(/hola/i)).toBeInTheDocument();
-	expect(screen.getByText(/hola/).closest("a")).toHaveAttribute("href", "/");
+	expect(screen.getByText(/step1/i)).toBeInTheDocument();
+	expect(screen.getByText(/step1/).closest("a")).toHaveAttribute("href", "/");
+	expect(screen.getAllByRole("link").length).toBe(steps.length);
 });
